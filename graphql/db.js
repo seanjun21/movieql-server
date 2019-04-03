ca0778002e9f6a4f9629cdcea5f5f7afd79b77df
Sean Jun <seanjun21@gmail.com>
2019-04-03 15:26:54 -0700

    Delete Mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,37 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: '0',
-    name: 'Sean',
-    age: 33,
-    gender: 'male'
+    id: 0,
+    name: 'Star Wars',
+    score: 1
   },
   {
-    id: '1',
-    name: 'Sora',
-    age: 34,
-    gender: 'female'
+    id: 1,
+    name: 'Avengers',
+    score: 8
   },
   {
-    id: '2',
-    name: 'Joe',
-    age: 25,
-    gender: 'male'
+    id: 2,
+    name: 'The Godfather I',
+    score: 99
   },
   {
-    id: '3',
-    name: 'Tad',
-    age: 22,
-    gender: 'male'
-  },
-  {
-    id: '4',
-    name: 'Bryan',
-    age: 36,
-    gender: 'male'
+    id: 3,
+    name: 'Logan',
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  }
+  movies.push(newMovie);
+  return newMovie
 };
